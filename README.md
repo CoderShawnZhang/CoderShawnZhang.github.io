b3922db7b13f7cfaacc9e60e2ac3e0b80404ad7f
zhanghongbo <shawnzhang@gmail.com>
2017-11-21 21:30:02 +0800

    vue-webpack

--- README.md
+++
@@ -1,1 +0,0 @@
-# CoderShawnZhang.github.io
---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template lang="jade">
+div
+  img(class="logo", src="../assets/logo.png")
+  h1 {{msg}}
+  comp-a
+  comp-b
+  counter
+</template>
+
+<script>
+import CompA from '../components/a.vue'
+import CompB from '../components/b.vue'
+import Counter from '../components/counter.vue'
+export default {
+  data () {
+    return {
+      msg: 'Hello from vue-loader!'
+    }
+  },
+  components: {
+    CompA,
+    CompB,
+    Counter
+  }
+}
+</script>
